5b99631f40f97db998a6ff5a18af6f53bd041d53
Aadarsh mishra <aadarshmishra2432007@gmail.com>
2025-12-02 10:38:39 +0530

    Add C++ example for basic data types
    Introduced Datatype.cpp demonstrating usage of integer, character, float, double, boolean, string, and void pointer types in C++. Useful for Day 01 lecture material.

---
+++ DSA/Lecture/Day01/Datatype.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+// Data type in c++
+int main() {
+    // Integer data type
+    int a = 10;
+    cout << "Integer: " << a << endl;
+    // Character data type
+    char b = 'A';
+    cout << "Character: " << b << endl;
+    // Float data type
+    float c = 5.75;
+    cout << "Float: " << c << endl;
+    // Double data type
+    double d = 19.99;
+    cout << "Double: " << d << endl;
+    // Boolean data type
+    bool e = true;
+    cout << "Boolean: " << e << endl;
+    // String data type
+    string f = "Hello, World!";
+    cout << "String: " << f << endl;
+    // Void data type (not directly printable)
+    void* g = nullptr; // Using a void pointer
+    cout << "Void pointer: " << g << endl;
+    return 0;
+}
